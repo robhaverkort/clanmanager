1e0b1eb0df518a9c7c383efcbc4d90c91c7a645d
Rob Haverkort <rob.haverkort@ziggo.nl>
2015-04-14 10:10:36 +0200

    Created basic security/login structure

--- src/ClanmanagerBundle/Controller/DefaultController.php
+++ src/ClanmanagerBundle/Controller/DefaultController.php
@@ -3,11 +3,23 @@
 namespace ClanmanagerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
-{
-    public function indexAction($name)
-    {
-        return $this->render('ClanmanagerBundle:Default:index.html.twig', array('name' => $name));
+class DefaultController extends Controller {
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction() {
+        return $this->render('ClanmanagerBundle:Default:index.html.twig');
     }
+
+    /**
+     * @Route("/admin")
+     */
+    public function adminAction() {
+        return new Response('Admin page!');
+    }
+
 }
