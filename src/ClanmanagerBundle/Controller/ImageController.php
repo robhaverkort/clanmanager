6c0fe302b9a0abacc443912220e797d217f83ed6
Rob Haverkort <rob.haverkort@ziggo.nl>
2015-06-17 10:12:48 +0200

    -images, added multiple regions

--- src/ClanmanagerBundle/Controller/ImageController.php
+++ src/ClanmanagerBundle/Controller/ImageController.php
@@ -33,31 +33,54 @@ class ImageController extends Controller {
     }
 
     /**
-     * @Route("/image/display/{image_id}", name="image_display")
+     * @Route("/image/display/{image_id}/{attack_id}/{part_id}", name="image_display")
      * @Security("has_role('ROLE_ADMIN')")
      */
-    public function displayAction($image_id) {
+    public function displayAction($image_id, $attack_id, $part_id) {
 
         $filename = "/srv/www/htdocs/clanmanager/web/images/images/testocr.png";
 
         $image = imagecreatefrompng($filename);
-        //$im_cropped = imagecrop($image, array(100,100,100,100));
 
-        $dest = imagecreatetruecolor(38, 24);
-        imagecopy($dest, $image, 0, 0, 204, 224, 38, 24); // my clan
-        imagecopy($dest, $image, 0, 0, 626, 224, 38, 24); // other clan
+        switch ($part_id) {
+
+            case 0:
+                $dest = imagecreatetruecolor(40, 28);
+                imagecopy($dest, $image, 0, 0, 204, 224 + $attack_id * 60, 38, 28); // my clan
+                break;
+            case 1:
+                $dest = imagecreatetruecolor(40, 28);
+                imagecopy($dest, $image, 0, 0, 626, 224 + $attack_id * 60, 38, 24); // other clan
+                break;
+            case 2:
+                $dest = imagecreatetruecolor(28, 18);
+                imagecopy($dest, $image, 0, 0, 180, 252 + $attack_id * 60, 28, 18); // other clan
+                break;
+            case 3:
+                $dest = imagecreatetruecolor(28, 18);
+                imagecopy($dest, $image, 0, 0, 605, 252 + $attack_id * 60, 28, 18); // other clan
+                break;
+            case 4:
+                $dest = imagecreatetruecolor(60, 20);
+                imagecopy($dest, $image, 0, 0, 215, 252 + $attack_id * 60, 60, 20); // other clan
+                break;
+            case 5:
+                $dest = imagecreatetruecolor(60, 20);
+                imagecopy($dest, $image, 0, 0, 640, 252 + $attack_id * 60, 60, 20); // other clan
+                break;
+            case 6:
+                $dest = imagecreatetruecolor(140, 16);
+                imagecopy($dest, $image, 0, 0, 862, 256 + $attack_id * 60, 140, 16); // other clan
+                break;
+        }
 
         $response = new Response();
-
         $response->headers->set('Cache-Control', 'private');
         $response->headers->set('Content-type', 'image/png');
         $response->headers->set('Content-Disposition', 'inline; filename="' . basename($filename) . '";');
         $response->headers->set('Content-length', filesize($filename));
-
-        $response->sendHeaders();
-        //$response->setContent(readfile($filename));
         $response->setContent(imagepng($dest, NULL, 0, PNG_NO_FILTER));
-        return;
+        return $response;
     }
 
 }
