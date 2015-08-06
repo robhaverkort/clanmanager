f107f6b253abd0628403536406fc95bbfd6f3a73
Rob Haverkort <rob.haverkort@ziggo.nl>
2015-08-07 00:34:59 +0200

    - edited dbupdate.sh

--- dbupdate.sh
+++ dbupdate.sh
@@ -4,3 +4,5 @@ mysqldump -u root clanmanager > clanmanager.sql
 
 ssh rhbv@www.rhbv.nl mysql --user=rhbv_clanmanager --password=clanmanager --database=rhbv_clanmanager < clanmanager.sql
 
+git commit -m "- dbupdate" clanmanager.sql
+
