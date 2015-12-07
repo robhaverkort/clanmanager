64e5c0a58c5f3d349caed93097a5e730a0fe624d
Rob Haverkort <rob.haverkort@ziggo.nl>
2015-12-07 09:29:48 +0100

    -clan_view, fixed for empty getPlayer()

--- src/ClanmanagerBundle/Controller/ClanController.php
+++ src/ClanmanagerBundle/Controller/ClanController.php
@@ -50,7 +50,9 @@ class ClanController extends Controller {
                 foreach ($warplayer->getAttacks() as $attkey => $attack) {
                     $w['players'][$warplayer->getPlayer()->getId()]['attacks'][$attkey]['stars'] = $attack->getStars();
                 }
-                $w['players'][$warplayer->getPlayer()->getId()]['netstars'] = $warplayer->getNetstars();
+                if ($warplayer->getPlayer()) {
+                    $w['players'][$warplayer->getPlayer()->getId()]['netstars'] = $warplayer->getNetstars();
+                }
             }
             $wars[] = $w;
         }
